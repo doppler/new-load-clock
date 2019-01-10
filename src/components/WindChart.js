0faee76e8a9af22a56d8453d888bf75094f24862
David Rose <doppler@gmail.com>
2019-01-09 22:17:58 -0500

    add vertical grads for time

--- src/components/WindChart.js
+++ src/components/WindChart.js
@@ -23,6 +23,7 @@ const Grad = ({ grad, max }) => (
     style={{ bottom: `${(grad / max) * 100}%` }}
   />
 );
+
 const Grads = ({ maxSpeed }) => {
   /* eslint-disable array-callback-return */
   return [5, 10, 15, 20, 25, 30, 35, 40, 45, 50].map(grad => {
@@ -31,11 +32,24 @@ const Grads = ({ maxSpeed }) => {
   /* eslint-enable */
 };
 
+const VertGrad = ({ grad }) => (
+  <div
+    className="VertGrad"
+    alt={`${grad} min`}
+    style={{ right: `${grad * 5}%` }}
+  />
+);
+
+const VertGrads = () => {
+  return [15, 10, 5].map(grad => <VertGrad key={grad} grad={grad} />);
+};
+
 export default ({ windSpeeds }) => {
   const maxSpeed = Math.max(...windSpeeds);
   return (
     <div className="WindChart">
       <Grads maxSpeed={maxSpeed} />
+      <VertGrads />
       {windSpeeds.map((speed, i) => (
         <Bar key={i} maxSpeed={maxSpeed} speed={speed} i={i} />
       ))}
