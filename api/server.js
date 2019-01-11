9b066fce23ec4c34d88c0e67981ba57ac1166503
David Rose <doppler@gmail.com>
2019-01-11 13:53:35 -0500

    setup express to serve app

--- api/server.js
+++ api/server.js
@@ -1,11 +1,14 @@
-const app = require("express")();
+const express = require("express");
+const app = express();
 const server = require("http").Server(app);
 const io = require("socket.io")(server);
+const path = require("path");
 
 server.listen(3001);
 
+app.use(express.static(path.join(__dirname, "../", "build")));
 app.get("/", function(req, res) {
-  res.sendFile(__dirname + "/index.html");
+  res.sendFile(path.join(__dirname, "..", "index.html"));
 });
 
 io.sockets.on("connection", socket => {
