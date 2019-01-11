e9e278e4083e9228920e19991349f7478260dd7d
David Rose <doppler@gmail.com>
2019-01-11 15:17:04 -0500

    change socket connection to 'window.location.origin'

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Compass from "./components/Compass";
 import WindChart from "./components/WindChart";
 import StatsTable from "./components/StatsTable";
 const io = require("socket.io-client");
-const socket = io(`http://${window.location.hostname}:5000`);
+const socket = io(window.location.origin);
 socket.open();
 
 const App = () => {
