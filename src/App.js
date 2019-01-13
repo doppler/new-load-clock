3dc6165234c19365def2d3ae1693c659a96afb50
David Rose <doppler@gmail.com>
2019-01-13 06:33:08 -0500

    -useRef

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from "react";
+import React, { useState, useEffect } from "react";
 import format from "date-fns/format";
 import "./App.css";
 import Compass from "./components/Compass";
