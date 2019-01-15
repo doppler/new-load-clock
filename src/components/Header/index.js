935832ca75269a0949925859e178b18ddd037712
David Rose <doppler@gmail.com>
2019-01-15 09:47:55 -0500

    nasty hack to enable nav to other locations

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { format } from "date-fns";
-import location from "../../lib/location";
+import location, { locationCode, locations } from "../../lib/location";
 
 export default () => {
   const [time, setTime] = useState("00:00:00 --");
@@ -13,9 +13,34 @@ export default () => {
     }, 1000);
     return () => clearInterval(timeInterval);
   }, []);
+
+  const navigateToOtherClock = direction => {
+    const locationKeys = Object.keys(locations);
+    let currentKeyIdx = locationKeys.indexOf(locationCode);
+    if (currentKeyIdx === 0 && direction === -1)
+      currentKeyIdx = locationKeys.length;
+    if (currentKeyIdx === locationKeys.length - 1 && direction === 1)
+      currentKeyIdx = -1;
+    window.location.pathname = `/${locationKeys[currentKeyIdx + direction]}`;
+  };
+  const [rangeValue, setRangeValue] = useState(0);
+  const handleRangeChange = event => {
+    setRangeValue(event.target.value);
+    navigateToOtherClock(Number(event.target.value));
+  };
   return (
     <div id="Header">
-      <div className="title">Skydive Spaceland {location}</div>
+      <div className="title">
+        Skydive Spaceland {location}
+        <input
+          onChange={handleRangeChange}
+          type="range"
+          min={-1}
+          max={1}
+          value={rangeValue}
+          step={1}
+        />
+      </div>
       <div className="time">{time}</div>
     </div>
   );
--- src/lib/location.js
+++ src/lib/location.js
@@ -1,5 +1,7 @@
 import validLocations from "../locations.json";
 
+export const locations = validLocations;
+
 export const getLocationCodeFromPathName = () => {
   const match = window.location.pathname.split("/")[1].match(/[A-Z]{3}/);
   if (match && Object.keys(validLocations).includes(match[0])) return match[0];
