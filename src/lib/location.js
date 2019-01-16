ee2c6c461c219134dbbe07f70dbc6fc8880817c0
David Rose <doppler@gmail.com>
2019-01-16 18:39:57 -0500

    add a bunch of location getters

--- src/lib/location.js
+++ src/lib/location.js
@@ -2,18 +2,29 @@ import validLocations from "../locations.json";
 
 export const locations = validLocations;
 
+export const getLocations = () => validLocations;
+
+export const allLocationCodes = Object.keys(validLocations);
+
+export const getLocationCodes = () => allLocationCodes;
+
 export const getLocationCodeFromPathName = () => {
-  const match = window.location.pathname.split("/")[1].match(/[A-Z]{3}/);
-  if (match && Object.keys(validLocations).includes(match[0])) return match[0];
+  const hash = window.location.hash.replace(/^#/, "");
+  console.log(hash);
+  if (allLocationCodes.includes(hash)) return hash;
   return "ATL";
 };
 
 export const locationCode = getLocationCodeFromPathName();
 
-export const allLocationCodes = Object.keys(validLocations);
+export const getLocationCode = () => locationCode;
 
 export const locationName = validLocations[locationCode]["name"];
 
+export const getLocationName = () => locationName;
+
 export const locationTimezone = validLocations[locationCode]["tz"];
+
+export const getLocationTimezone = () => locationTimezone;
 
 export default validLocations[locationCode];
