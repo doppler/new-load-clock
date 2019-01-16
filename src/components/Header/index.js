679b459fbe81b8e548b509e26f6c918d3e0315ad
David Rose <doppler@gmail.com>
2019-01-16 18:40:14 -0500

    use new location getters in Header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,10 +1,10 @@
 import React, { useState, useEffect } from "react";
 import moment from "moment-timezone";
 import {
-  locationName,
-  locationCode,
-  locationTimezone,
-  allLocationCodes
+  getLocationName,
+  getLocationCode,
+  getLocationTimezone,
+  getLocationCodes
 } from "../../lib/location";
 import "./Header.scss";
 
@@ -16,6 +16,9 @@ const NavArrow = ({ direction, onClick }) => (
 );
 
 export default () => {
+  const locationName = getLocationName();
+  const locationTimezone = getLocationTimezone();
+
   const [time, setTime] = useState("00:00:00 --");
   const updateTime = () => {
     setTime(moment.tz(locationTimezone).format("h:mm:ss A z"));
@@ -28,8 +31,9 @@ export default () => {
   }, []);
 
   const navigateToOtherClock = direction => {
+    const allLocationCodes = getLocationCodes();
     // const locationKeys = Object.keys(locations);
-    let currentKeyIdx = allLocationCodes.indexOf(locationCode);
+    let currentKeyIdx = allLocationCodes.indexOf(getLocationCode());
     if (currentKeyIdx === 0 && direction === -1)
       currentKeyIdx = allLocationCodes.length;
     if (currentKeyIdx === allLocationCodes.length - 1 && direction === 1)
