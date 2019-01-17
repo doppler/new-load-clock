fd118c5ea294e7f82fba4e28b4c3ddbc9f86f484
David Rose <doppler@gmail.com>
2019-01-17 14:38:38 -0500

    add jwt and only listen to backend messages sent with jwt

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
 const server = require("http").Server(app);
+const jwt = require("jsonwebtoken");
 const io = require("socket.io")(server);
 const path = require("path");
 const locations = require("./src/locations.json");
@@ -29,13 +31,38 @@ io.sockets.on("connection", socket => {
     socket.emit("load-announcment", []);
     io.to(location).emit("load-announcement", loadAnnouncements[location]);
   });
-  socket.on("weather-record", record => {
-    console.log("weather-record", record.location, record.time);
-    io.to(record.location).emit("weather", record);
+  socket.on("jwt-weather-record", token => {
+    jwt.verify(token, process.env.JWT_SECRET, (err, record) => {
+      if (err) {
+        console.log(err.message);
+        return false;
+      }
+      console.log("jwt-weather-record", record.location, record.time);
+      io.to(record.location).emit("weather", record);
+    });
   });
-  socket.on("load-announcement", announcement => {
-    console.log("loads", announcement.location, announcement.time);
-    loadAnnouncements[announcement.location] = announcement;
-    io.to(announcement.location).emit("load-announcement", announcement);
+  // socket.on("weather-record", record => {
+  //   console.log("weather-record", record.location, record.time);
+  //   io.to(record.location).emit("weather", record);
+  // });
+  socket.on("jwt-load-announcement", token => {
+    jwt.verify(token, process.env.JWT_SECRET, (err, announcement) => {
+      if (err) {
+        console.log(err.message);
+        return false;
+      }
+      console.log(
+        "jwt-load-announcment",
+        announcement.location,
+        announcement.time
+      );
+      loadAnnouncements[announcement.location] = announcement;
+      io.to(announcement.location).emit("load-announcement", announcement);
+    });
   });
+  // socket.on("load-announcement", announcement => {
+  //   console.log("loads", announcement.location, announcement.time);
+  //   loadAnnouncements[announcement.location] = announcement;
+  //   io.to(announcement.location).emit("load-announcement", announcement);
+  // });
 });
