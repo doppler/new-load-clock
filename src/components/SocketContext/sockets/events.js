97f8f274e2e4ffb9254f98593212c979b8f61643
David Rose <doppler@gmail.com>
2019-01-18 15:37:15 -0500

    console debug on socket msg

--- src/components/SocketContext/sockets/events.js
+++ src/components/SocketContext/sockets/events.js
@@ -2,11 +2,13 @@ import { socket } from "./";
 
 export const socketEvents = ({ setValue }) => {
   socket.on("weather", weather => {
+    console.debug(new Date().toString(), "'weather' from socket");
     setValue(state => {
       return { ...state, weather };
     });
   });
   socket.on("load-announcement", announcement => {
+    console.debug(new Date().toString(), "'load-announcement' from socket");
     const { loads } = announcement;
     setValue(state => {
       return { ...state, loads };
