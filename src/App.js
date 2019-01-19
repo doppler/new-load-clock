05433998a0f7355186141588d8900428e6062290
David Rose <doppler@gmail.com>
2019-01-19 06:54:53 -0500

    on scroll, snap to nearest screen

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Screen from "./components/Screen";
 import "./App.scss";
 import locations from "./locations.json";
+
 const App = () => {
+  const [clientWidth, setClientWidth] = useState(document.body.clientWidth);
+  // const [pageXOffset, setPageXOffset] = useState(window.pageXOffset);
+  useEffect(() => {
+    window.onresize = () => {
+      setClientWidth(document.body.clientWidth);
+    };
+    let timer;
+    window.onscroll = () => {
+      if (timer) clearTimeout(timer);
+      timer = setTimeout(() => {
+        const offset = Math.round(window.pageXOffset / clientWidth);
+        window.scrollTo(clientWidth * offset, 0);
+      }, 75);
+    };
+  }, []);
   return (
     <div className="App">
       <div className="Carousel">
