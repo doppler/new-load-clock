49c9e264f70a0dd91c4d1e9964981f07f86deb4e
David Rose <doppler@gmail.com>
2019-01-20 08:23:28 -0500

    Add wind speed averages

--- src/components/Footer/WindWidget/WindStatsTable/index.js
+++ src/components/Footer/WindWidget/WindStatsTable/index.js
@@ -1,31 +1,41 @@
 import React from "react";
 import "./StatsTable.scss";
 
-const windSpeedAvg = (windspeeds, timespan) => {
-  const seconds = timespan * 60;
-  const ticks = seconds / 2; // service reports every 2 seconds
-  const sum = windspeeds.slice(0, ticks).reduce((a, b) => a + b);
+let windSpeeds;
+
+const windSpeedHigh = timespan => {
+  return Math.max(...windSpeeds.slice(0, (timespan * 60) / 2));
+};
+
+const windSpeedAvg = timespan => {
+  if (!windSpeeds.length) return "...";
+  const ticks = (timespan * 60) / 2; // service reports every 2 seconds
+  const slice = windSpeeds.slice(0, ticks);
+  const sum = slice.reduce((a, b) => a + b);
   return Math.round(sum / ticks);
 };
 
-const StatsRow = ({ minute, speed, avg }) => (
-  <tr style={{ color: `hsl(${135 - speed * 6}, 100%, 50%)` }}>
-    <td>{minute}min high:</td>
-    <td>{speed !== -Infinity ? speed : "..."}</td>
-    <td>{avg}</td>
-  </tr>
-);
+const colorForSpeed = speed => {
+  return `hsl(${135 - speed * 6}, 100%, 50%)`;
+};
+const StatsRow = ({ minute }) => {
+  const high = windSpeedHigh(minute);
+  const avg = windSpeedAvg(minute);
+  return (
+    <tr>
+      <td>{minute} minute</td>
+      <td style={{ color: colorForSpeed(avg) }}>{avg}</td>
+      <td style={{ color: colorForSpeed(high) }}>{high}</td>
+    </tr>
+  );
+};
 
 export default ({ weather }) => {
   if (!weather.prevWindSpeeds)
     return <div className="StatsTable">No Weather Data.</div>;
-  const windSpeeds = weather.prevWindSpeeds
+  windSpeeds = weather.prevWindSpeeds
     ? [...weather.prevWindSpeeds].reverse()
     : [];
-  const wind5minHigh = Math.max(...windSpeeds.slice(0, 150));
-  const wind10minHigh = Math.max(...windSpeeds.slice(150, 300));
-  const wind20minHigh = Math.max(...windSpeeds.slice(300, 600));
-
   return (
     <div className="StatsTable">
       <table>
@@ -36,26 +46,19 @@ export default ({ weather }) => {
             }}
           >
             <td>Temperature:</td>
-            <td>
+            <td colSpan={2}>
               {weather.outsideTemp ? `${weather.outsideTemp}` : "..."}&deg;F
             </td>
             <td />
           </tr>
-          <StatsRow
-            minute={5}
-            speed={wind5minHigh}
-            avg={windSpeedAvg(windSpeeds, 5)}
-          />
-          <StatsRow
-            minute={10}
-            speed={wind10minHigh}
-            avg={windSpeedAvg(windSpeeds, 10)}
-          />
-          <StatsRow
-            minute={20}
-            speed={wind20minHigh}
-            avg={windSpeedAvg(windSpeeds, 20)}
-          />
+          <tr style={{ fontSize: "0.5rem" }}>
+            <td />
+            <td>Avg</td>
+            <td>High</td>
+          </tr>
+          {[5, 10, 20].map(i => (
+            <StatsRow key={i} minute={i} />
+          ))}
         </tbody>
       </table>
     </div>
