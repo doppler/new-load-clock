e63a3b040200bcc76e6d99fd505a006e9232905c
David Rose <doppler@gmail.com>
2020-01-13 09:11:35 -0500

    a better way to manage dynamic font sizes: vh units!!

--- src/App.js
+++ src/App.js
@@ -28,20 +28,11 @@ const App = () => {
   const [menuVisible, setMenuVisible] = useState(false);
   const toggleMenu = () => setMenuVisible(!menuVisible);
   useEffect(() => {
-    let vh = window.innerHeight * 0.01;
-    document.documentElement.style.setProperty("--vh", `${vh}px`);
-  }, []);
-  useEffect(() => {
     const hashChangeListener = window.addEventListener("hashchange", event => {
       setLocationCode(codeFromUrl());
     });
-    // const windowSizeListener = window.addEventListener("resize", () => {
-    //   let vh = window.innerHeight * 0.01;
-    //   document.documentElement.style.setProperty("--vh", `${vh}px`);
-    // });
     return () => {
       window.removeEventListener("hashchange", hashChangeListener);
-      // window.removeEventListener("resize", windowSizeListener);
     };
   }, []);
   const { weather, loads } = useContext(SocketContext);
